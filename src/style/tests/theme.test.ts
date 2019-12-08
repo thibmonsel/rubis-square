6b12d947472994198f4d643c9b894f4ffb1d1056
sebastien-lb <sebast.lubineau@laposte.net>
2019-12-08 21:18:23 +0100

    refactor: remove useless code

--- src/style/tests/theme.test.ts
+++ src/style/tests/theme.test.ts
@@ -1,18 +1,7 @@
-import { GameDefinition } from '../../services/game-service';
 import { getGridDisplay } from '../theme';
 
 describe('Theme function test', (): void => {
   describe('getGridDisplay should return the correct display', (): void => {
-    const game: GameDefinition = {
-      nbColor: 3,
-      board: [
-        [0, 1, 2, 1, 2],
-        [0, 1, 2, 1, 2],
-        [0, 1, 2, 1, 2],
-        [0, 1, 2, 1, 2],
-        [0, 1, 2, 1, 2],
-      ],
-    };
     const testsCases = [
       {
         game: {
